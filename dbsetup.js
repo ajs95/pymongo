bbefb81e861b74a56d2764eef902f4f76454f949
Anand J S <34867139+ajs95@users.noreply.github.com>
2019-11-30 23:47:48 +0530

    Update and rename data.js to dbsetup.js

--- dbsetup.js
+++ dbsetup.js
@@ -2,3 +2,5 @@ conn = new Mongo();
 db = conn.getDB("myDatabase");
 db.peopletest.save({city:"Mumbai", country:"India", language: "Hindi"});
 db.peopletest.save({city:"Pune", country:"India", language: "Hindi"});
+db = conn.getDB("admin");
+db.createUser({ user: "ajsadmin", pwd: "ajsadmin", roles: [ { role: "root", db: "admin" } ]});
